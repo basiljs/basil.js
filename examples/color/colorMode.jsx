a3524054a1fdaeff56f68f032f6f8a8b54714a6d
Benedikt Groß <benedikt@looksgood.de>
2012-05-26 15:51:24 +0100

    unit() function, renaming of all internals vars with prefix '_', currentLayer(), new vars for color/stroke/tint state

--- examples/color/colorMode.jsx
+++ examples/color/colorMode.jsx
@@ -1,1 +1,1 @@
-﻿#include "../../basil.js";function draw() {  b.colorMode( b.CMYK );  b.text('hello basil.js', 0, 0, 300, 300);}
+#include "../../basil.js";function draw() {  b.colorMode( b.CMYK );  b.text('hello basil.js', 10, 20, 300, 100);  b.rect(0, 0, 500, 400);}
