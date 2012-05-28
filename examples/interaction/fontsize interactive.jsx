981fea44093b3c07b46df2fadfe301e33cb54e0b
Benedikt Groß <benedikt@looksgood.de>
2012-05-28 21:41:15 +0100

    renamed

--- examples/interaction/fontsize interactive.jsx
+++ examples/interaction/fontsize interactive.jsx
@@ -2,13 +2,22 @@
 #include "../../basil.js";
 
 function setup() {
-  var idleTask = app.idleTasks.add({name:"create_circles_idle_task", sleep:3});
+  var idleTask = app.idleTasks.add({name:"create_circles_idle_task", sleep:10});
   var onIdleEventListener = idleTask.addEventListener(IdleEvent.ON_IDLE, onIdleEventHandler, false);
+
   alert("Created idle task " + idleTask.name + "; Run the _Stop() script to end it!");
-  b.text("move me up/down",-250,0, 50,30);
+
+  var ctrl = b.text("move me",-250,100, 50,30);
+  ctrl.label = "ctrl";
+  var textFramePage = b.text("basel", 0,0,b.width,b.height);
+  textFramePage.label = "target";
 }
 
-function onIdleEventHandler(myIdleEvent) {
-  var textFrame = app.activeDocument.textFrames.item(0); 
-  textFrame.contents = Math.random()+"";
+function onIdleEventHandler(idleEvent) {
+  var ctrl = b.findByLabel("ctrl")[0];
+  var textFramePage = b.findByLabel("target")[0];
+  var y = ctrl.geometricBounds[0];
+  textFramePage.contents = "basel "+y;
+  textFramePage.paragraphs[0].pointSize = y;
+  b.println("onIdleEventHandler "+y);
 }
