0b4f0d4a55e5f06d736f8fdb54f2e997bd9f97f0
Benedikt Groß <benedikt@looksgood.de>
2012-05-29 15:41:14 +0100

    please ignore

--- examples/target engine test.jsx
+++ examples/target engine test.jsx
@@ -1,5 +1,12 @@
 #include "../basil.js";
+b.println($.engineName);
+var counter = 0;
+for(var prop in this) {
+    if(this.hasOwnProperty(prop)) {
+      b.println(counter+"  "+prop);
+      counter++;
+    }
+}
 
 //var stefan = "stefan";
-
 b.println(stefan);
