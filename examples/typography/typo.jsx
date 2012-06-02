dd6e9f8fc85bdd0ef91e50f15859a4e6a6eff0e5
Stefan Landsbek <stefan.landsbek@gmail.com>
2012-06-02 10:50:46 +0200

    more sophisticated typo example

--- examples/typography/typo.jsx
+++ examples/typography/typo.jsx
@@ -1,16 +1,40 @@
 #include "../../basil.js";
 
 function draw() {
-  var green = b.color(0, 255, 0, "green");
-  var red = b.color(255, 0, 0, "red");
-  b.fill(green);
   var content = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr,\r";
   content += "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.";
-  var textFrame = b.text(content, 0, 0, 600, 600);
+
+  var doc = b.doc();
+
+  // add 5 pages to doc
+  for (var i = 0; i < 5; i++) {
+    doc.pages.add();  
+  }
   
-  b.typo(b.doc(), 'pointSize', 48);
-  b.typo(textFrame.words[1], 'fillColor', red);
-  b.typo(textFrame.lines[1], 'pointSize', 24);
+  // add 2 text frame on each page
+  for (var i = 0, len = doc.pages.length; i < len; i++) {
+    b.page(i);
+    b.text(content, 0, 0, 600, 300);
+    b.text(content, 0, 350, 600, 300);
+  }
+  
+  var green = b.color(0, 255, 0, "green");
+  var red = b.color(255, 0, 0, "red");
+
+  // change pointSize in all text frames in document
+  b.typo(doc, 'pointSize', 48);
+
+  // change fillColor on 2nd spread
+  b.typo(doc.spreads[1], 'fillColor', red);
+
+  // change strokeColor on 4th page
+  b.typo(doc.pages[3], 'strokeColor', green);
+
+  // change some more fine granular stuff in text frame on 1st page
+  var textFrame = doc.pages[0].textFrames[0];
+  b.typo(textFrame, 'pointSize', 24);
+  b.typo(textFrame.words[1], 'fillColor', green);
+  b.typo(textFrame.lines[1], 'pointSize', 48);
   b.typo(textFrame.characters[50], 'pointSize', 96);
   b.typo(textFrame.paragraphs[1], 'strokeColor', red);
 }
