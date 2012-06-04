2ebee6f6e0faf05394e66b0677897ac176017443
Benedikt Groß <benedikt@looksgood.de>
2012-06-04 12:44:46 +0100

    new examples for fillTint() and strokeTint()

---
+++ examples/color/fillTint.jsx
@@ -0,0 +1,15 @@
+#include "../../basil.js";
+
+function draw() {
+  var counter = 50;
+  b.noStroke();
+  var rectHeight = b.height/counter;
+
+  for (var i = 0; i < counter; i++) {
+    var y = b.map(i, 0,counter-1, 0,b.height-rectHeight);
+    var fillTint = b.map(i, 0,counter-1, 100,0);
+
+    b.fillTint( fillTint );
+    b.rect(0,y, b.width,rectHeight);
+  };
+};
