0b1b8f294149e4da695b25c0330c9c3f62a39d00
Benedikt Groß <benedikt@looksgood.de>
2012-06-06 10:39:50 +0100

    tmp … please ignore

--- examples/transform/tmp_transforms_test.jsx
+++ examples/transform/tmp_transforms_test.jsx
@@ -1,14 +1,21 @@
 #include "../../basil.js";
 
 function draw() {
-  //b.translate(36,36);
-  //b.scale(2);
+  b.noStroke();
   //b.printMatrix();
-  //b.rotate(5);
+  b.translate(10,36);
+  b.scale(2,1.5);
+  b.rotate( b.radians(5) );
+  //b.shearX( b.radians(33) );
+  b.printMatrix();
   
-  var matrix = [2,-0.1743,0.1743,2,36,36];
+  // 1,0,36,0,1,36
+  //var matrix = [2,-0.1743,0.1743,2,36,36];
+  //var matrix = [2,-0.1743,1.1547,2,36,36];
   var rect = b.rect(0,0,100,100);
+  /*
   rect.transform(CoordinateSpaces.PASTEBOARD_COORDINATES,
                    AnchorPoint.TOP_LEFT_ANCHOR,
                    matrix);
+   */
 };
