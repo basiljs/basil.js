990a5ec3f96a4f4ed9089ec323bb97abe071c33e
ffd8 <ted@ffd8.org>
2012-11-22 21:52:37 +0100

    removed setup(){ } function from template = no need to explain scoping just yet...

--- examples/typography/text.jsx
+++ examples/typography/text.jsx
@@ -1,18 +1,1 @@
-#includepath "~/Documents/;%USERPROFILE%Documents";
-#include "basiljs/bundle/basil.js";
-
-function draw() {
-  b.fill(b.color(0, 255, 0, "green"));
-  b.textFont('Helvetica','Bold');
-  b.textSize(96);
-  b.text('hello basil.js', 0, 0, 400, 300);
-
-  b.fill(b.color(255, 0, 0, "red"));
-  b.textAlign(Justification.RIGHT_ALIGN, VerticalJustification.BOTTOM_ALIGN);
-  b.textLeading(72);
-  b.textKerning(96);
-  b.textTracking(96);
-  b.text('hello basil.js', 0, 400, 400, 400);
-}
-
-b.go();
+﻿#includepath "~/Documents/;%USERPROFILE%Documents";#include "basiljs/bundle/basil.js";function draw() {  var myString = "i love \n using variables \n in basil.js"; // note the \n for a newline    b.fill(b.color(10, 20, 30, "blackmagic"));  b.textFont('Helvetica','Bold');  b.textSize(96);  b.text('why hello basil.js', 0, 0, 350, 350);  b.fill(b.color(255, 25, 25, "redspecial"));  b.textAlign(Justification.RIGHT_ALIGN, VerticalJustification.CENTER_ALIGN);  b.textLeading(150);  b.textTracking(1000);  b.textSize(24);  b.text(myString, 0, b.height/2, b.width, b.height/2);}b.go();
