34a62bbb76f0f2cd9b6bbc7371c070d28564e2a3
ffd8 <ted@ffd8.org>
2012-11-23 02:59:11 +0100

    modifications to the text examples

--- examples/typography/textAlign.jsx
+++ examples/typography/textAlign.jsx
@@ -1,45 +1,1 @@
-#includepath "~/Documents/;%USERPROFILE%Documents";
-#include "basiljs/bundle/basil.js";
-
-function draw() {
-  /*
-  Justification.AWAY_FROM_BINDING_SIDE
-  Justification.CENTER_ALIGN
-  Justification.CENTER_JUSTIFIED
-  Justification.FULLY_JUSTIFIED
-  Justification.LEFT_ALIGN
-  Justification.RIGHT_ALIGN
-  Justification.RIGHT_JUSTIFIED
-  Justification.TO_BINDING_SIDE
-  */
- 
-  /*
-  VerticalJustification.BOTTOM_ALIGN
-  VerticalJustification.CENTER_ALIGN
-  VerticalJustification.JUSTIFY_ALIGN
-  VerticalJustification.TOP_ALIGN
-   */
-  var black = b.color("Black");
-  var red = b.color(255,0,0);
-  var green = b.color(0,255,0);
-
-  b.fill( black );
-  b.textFont('Helvetica\tBold');
-  b.textSize(48);
-
-  b.text('hello basel', 0, 0, b.width, 80);
-
-  b.fill( red);
-  b.textAlign(Justification.CENTER_ALIGN);
-  b.text('hello basel', 0, 100, b.width, 80);
-
-  b.fill( red);
-  b.textAlign(Justification.CENTER_JUSTIFIED);
-  b.text('hello basel', 0, 200, b.width, 80);
-
-  b.fill( green);
-  b.textAlign(Justification.RIGHT_ALIGN, VerticalJustification.TOP_ALIGN);
-  b.text('hello basel', 0, 300, b.width, 80);
-}
-
-b.go();
+﻿#includepath "~/Documents/;%USERPROFILE%Documents";#include "basiljs/bundle/basil.js";function draw() {  /*  Justification.AWAY_FROM_BINDING_SIDE  Justification.CENTER_ALIGN  Justification.CENTER_JUSTIFIED  Justification.FULLY_JUSTIFIED  Justification.LEFT_ALIGN  Justification.RIGHT_ALIGN  Justification.RIGHT_JUSTIFIED  Justification.TO_BINDING_SIDE  VerticalJustification.BOTTOM_ALIGN  VerticalJustification.CENTER_ALIGN  VerticalJustification.JUSTIFY_ALIGN  VerticalJustification.TOP_ALIGN   */ var sayThis = "basil.js";var changeCol = 100;var changeVal = 10;var box;var strokeW = .25;  b.fill( changeCol);  b.textFont('Helvetica', 'Bold');  b.textSize(48);  box = b.text(sayThis, 0, 0, b.width, 80);  changeCol -= changeVal;  box.strokeWeight=strokeW;  b.fill( changeCol);  b.textAlign(Justification.CENTER_ALIGN);  box = b.text(sayThis, 0, 100, b.width, 80);  changeCol -= changeVal;  box.strokeWeight=strokeW;    b.fill( changeCol);  b.textAlign(Justification.RIGHT_ALIGN);  box = b.text(sayThis, 0, 200, b.width, 80);  changeCol -= changeVal;  box.strokeWeight=strokeW;    b.fill( changeCol);  b.textAlign(Justification.FULLY_JUSTIFIED);  box = b.text(sayThis, 0, 300, b.width, 80);  changeCol -= changeVal;  box.strokeWeight=strokeW;    b.fill( changeCol);  b.textAlign(Justification.FULLY_JUSTIFIED, VerticalJustification.CENTER_ALIGN);  box = b.text(sayThis, 0, 400, b.width, 80);  changeCol -= changeVal;  box.strokeWeight=strokeW;    b.fill( changeCol);  b.textAlign(Justification.FULLY_JUSTIFIED, VerticalJustification.BOTTOM_ALIGN);  box = b.text(sayThis, 0, 500, b.width, 80);  box.strokeWeight=strokeW;}b.go();
