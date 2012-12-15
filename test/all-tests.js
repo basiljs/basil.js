2e67e91fa84d9238125dcb982be24d136607e7b3
Stefan Landsbek <stefan.landsbek@gmail.com>
2012-12-15 13:56:29 +0100

    introduction of test suite for all tests

---
+++ test/all-tests.js
@@ -0,0 +1,6 @@
+#include "../basil.js";
+#include "../lib/basil.test.js";
+
+#include "typography-tests.jsx";
+#include "transform-tests.jsx";
+// TODO include all other test files after includes in header are conditional like in typography-tests.jsx...
