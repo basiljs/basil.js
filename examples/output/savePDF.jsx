1fb4cb1b715fe7b2f99203cbb4425a1c6099bd5c
ludzeller <ludwig.zeller@fhnw.ch>
2012-12-20 23:40:52 +0100

    Improved savePDF example

--- examples/output/savePDF.jsx
+++ examples/output/savePDF.jsx
@@ -2,9 +2,15 @@
 #include "basiljs/bundle/basil.js";
 
 function draw() {
-  b.textSize(128);
-  b.text("hello pdf", 0,0, 500, 300);
-  b.savePDF("hello.pdf");
+  
+  b.textSize(64);
+  var tf = b.text("this content will be overwritten", b.width/2-250,b.height/2-150, 500, 300);
+  
+  for( var i = 0; i < 20; i++) {
+    tf.contents = "hello pdf " + i;
+    b.savePDF("hello-pdf-" + i + ".pdf");
+  }
+  
   // show export options
   //b.savePDF("hello.pdf",true);
 };
