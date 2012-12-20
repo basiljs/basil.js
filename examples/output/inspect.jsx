7e2ce459a027d143410d5bd7867ca43da58d715b
Benedikt Gross <benedikt@looksgood.de>
2012-12-20 09:18:41 +0100

    added inspect() for debugging nested objects + example

---
+++ examples/output/inspect.jsx
@@ -0,0 +1,15 @@
+#includepath "~/Documents/;%USERPROFILE%Documents";
+#include "basiljs/bundle/basil.js";
+
+function draw() {
+  b.clear(b.doc());
+
+  var textFrame = b.text("hi!", 0,0,50,50);
+  var someObject = {prop:"hello", fun:function(){}, num:5, arr:[0,1,2,3,4,5,function(){}], c:new Color()};
+
+  b.inspect( someObject );
+  //b.inspect( textFrame );
+  //b.inspect( b.doc().allPageItems );
+}
+
+b.go();
