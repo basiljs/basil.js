f4232c3ec04893660161844664430f41b26a807f
ffd8 <ted@ffd8.org>
2013-02-06 18:12:32 +0100

    added new benchmark test

---
+++ test/benchmarks/line_over_word.jsx
@@ -0,0 +1,57 @@
+﻿#includepath "~/Documents/;%USERPROFILE%Documents";
+#include "basiljs/bundle/basil.js";
+
+var numPages = 10;
+var textSize = 20;
+var pageTime = 0;
+var textLayer = b.layer("text");
+var lineLayer = b.layer("lines");
+
+function draw() {
+  
+  for(var i = 0; i < numPages; i++){
+    textLines();
+    if(i < numPages-1){
+      b.addPage();
+    } 
+    b.println("page " + (i + 1) + " took " + ((b.millis() - pageTime)/1000) + " seconds to process");
+    pageTime = b.millis();
+  }
+  textLayer.visible = false;
+  b.println("total script took " + (pageTime/1000) + " seconds to process");
+}
+
+function textLines(){
+  // build textframe with lorem text to margin, 
+  b.canvasMode(b.MARGIN);
+  b.layer(textLayer);
+  b.textSize(textSize);
+  var myTextFrame = b.text(" ",0,0,b.width,b.height);
+  app.documents[0].stories.everyItem().hyphenation = false; 
+  myTextFrame.contents = TextFrameContents.placeholderText;
+
+  // run function with fullpage mode
+  b.canvasMode(b.PAGE); 
+  b.layer(lineLayer);
+  b.paragraphs(myTextFrame, function(p,n){
+    p.hyphenation = false;
+    b.lines(p, function(l,m){
+      var wbp; // temp bounds
+      b.words(l, function(w,o){
+        var wb = b.bounds(w);
+        if(wbp == undefined){
+          b.line(0,wb.bottom,wb.left,wb.bottom);
+        }else{
+          b.line(wbp.right,wb.bottom,wb.left,wb.bottom);
+        }
+        b.line(wb.left,wb.bottom,wb.left,wb.top);
+        b.line(wb.left,wb.top,wb.right,wb.top);
+        b.line(wb.right,wb.top,wb.right,wb.bottom);
+        wbp = wb;
+      });
+      b.line(wbp.right,wbp.bottom,b.width,wbp.bottom); // last to right edge
+    });
+  });
+}
+
+b.go();
