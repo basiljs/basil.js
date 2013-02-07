da2fc8d1fe7a2a0dcd5a419a32d102afb6287c5b
ludzeller <ludwig.zeller@fhnw.ch>
2013-02-07 16:20:41 +0100

    Speed improvements on real-time Socket interaction
    - fixed a bug where a connected client would wait 10 seconds for a new
    line, causing InDesign to stall
    - It's a good workaround but InDesign is definitely not designed for
    real time interaction

--- examples/loop/server.jsx
+++ examples/loop/server.jsx
@@ -2,32 +2,25 @@
 #includepath "~/Documents/;%USERPROFILE%Documents";
 #include "basiljs/bundle/basil.js";
 
-// really bad performance... :/
-// try with cs6
-// how is benedikt doing with it?
-// how is the client implementation working? processing as the server...
-
 // note: use telnet on terminal to send strings after the server was started
+// telnet localhost 1024
+// then type and press return
 
 var server = null;
 var conn = null;
-var textBox = null;
+    
+var tWidth;
+var tHeight;
 
 function setup() {
     b.doc();
     server = new Socket();
     server.listen(1024);
-    server.timeout = 99999;
+    server.timeout = 999999;
     b.println("server started at localhost 1024");
     
-    var tWidth = b.width - 150;
-    var tHeight = 400;
-    
-    textBox = b.text("server started at localhost 1024", b.width / 2 - tWidth / 2, b.height / 2 - tHeight / 2, tWidth, tHeight);
-    
-    b.typo(textBox, 'appliedFont', 'Helvetica\tBold');  
-    b.typo(textBox, 'pointSize', '72');   
-    b.typo(textBox, 'justification', Justification.CENTER_ALIGN);    
+    tWidth = b.width - 150;
+    tHeight = 400;
 };
 
 function draw() {
@@ -35,23 +28,28 @@ function draw() {
       b.println("poll loop");
 
       if(conn == null) conn = server.poll(); 
-      
-      if( conn != null && conn.connected ) {
+      if(conn != null && conn.connected) {
+        conn.timeout = 0.05; // time for connected client to wait for linefeed, otherwise blocks InDesign for 10 seconds
         var s = conn.readln();
         if ( s != null && s != "" ) {
           b.println( s );
-          textBox.contents = s;
+          
+          b.addPage();
+          var tf = b.text(s, b.width / 2 - tWidth / 2, b.height / 2 - tHeight / 2, tWidth, tHeight);
+          b.typo(tf, 'appliedFont', 'Helvetica\tBold');  
+          b.typo(tf, 'pointSize', '72');   
+          b.typo(tf, 'justification', Justification.CENTER_ALIGN);  
         }
       }
   
 };
 
 function cleanUp() {
-    server.close();
+    if(conn != null)conn.close();
+    if(server != null)server.close();
     server = null;
     conn = null;
     b.println("stopped server");
-    textBox.remove();
 };
 
-b.loop(3); 
+b.loop(15); 
