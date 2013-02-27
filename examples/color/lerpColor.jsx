4789ea2c6a4d45ec4c6a3cb9064be819845c1ad9
Benedikt Gross <benedikt@looksgood.de>
2013-02-27 17:22:28 +0000

    cleanup example of lerpColor()

--- examples/color/lerpColor.jsx
+++ examples/color/lerpColor.jsx
@@ -2,8 +2,8 @@
 #include "basiljs/bundle/basil.js";
 
 function draw() {
-  var c1 = b.color(0,0,0,0);
-  var c2 = b.color(100,100,0,0);
+  var c1 = b.color(0,255,0);
+  var c2 = b.color(255,128,0);
 
   var counter = 10;
   b.noStroke();
@@ -11,8 +11,9 @@ function draw() {
 
   for (var i = 0; i < counter; i++) {
     var y = b.map(i, 0,counter-1, 0,b.height-rectHeight);
+    var amt = b.map(i,0,counter-1,0,1);
 
-    b.fill( b.lerpColor(c1,c2,i/(counter-1)) );
+    b.fill( b.lerpColor(c1,c2,amt) );
     b.rect(0,y, b.width,rectHeight);
   }
 }
