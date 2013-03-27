4c0bbd85b5d9b37c2941c623e593aa636b18ae81
ffd8 <ted@ffd8.org>
2013-03-27 01:14:15 +0100

    added two typo demos

---
+++ examples/demos/B05_typographically_skewed/B05_typographically_skewed.jsx
@@ -0,0 +1,38 @@
+﻿#includepath "~/Documents/;%USERPROFILE%Documents";
+#include "basiljs/bundle/basil.js";
+
+function draw() {
+
+  var mySel = b.selections()[0];
+  
+  b.paragraphs(mySel, function(p, pn){
+    
+    var wordCount = b.words(p).length;
+    
+    b.words(p, function(w, n){
+      
+      var skew = b.map(n, 0, wordCount, -55,55); // -85, 85 is max deg°
+
+      //  have fun using ONE of the following lines below:
+//~         if(n < wordCount/2){
+//~         if(n%2==0){
+        if(n<skew){    
+          
+            b.typo(w, "skew", skew);
+          }else{
+            b.typo(w, "skew", -skew);
+          }
+        
+//~         b.typo(w, "skew", b.random(-75,75)); // all skewed up
+          
+      //  turn off code above to just break up line
+//~         b.typo(w, "baselineShift", -b.random(-10,10)); // cloud cluster of words
+
+        
+        
+    });
+  
+  });
+}
+
+b.go();
---
+++ examples/demos/B06_lines_over_words/B06_lines_over_words.jsx
@@ -0,0 +1,31 @@
+﻿#includepath "~/Documents/;%USERPROFILE%Documents";
+#include "basiljs/bundle/basil.js";
+
+function draw() {
+  
+  var myTextFrame = b.selections()[0];
+  var lineLayer = b.layer("lines");
+
+  b.layer(lineLayer);
+  b.paragraphs(myTextFrame, function(p,n){
+    p.hyphenation = false;
+    b.lines(p, function(l,m){
+      var wbp; // temp bounds
+      b.words(l, function(w,o){
+        var wb = b.bounds(w);
+        if(wbp == undefined){
+          b.line(0,wb.bottom,wb.left,wb.bottom);
+        }else{
+          b.line(wbp.right,wbp.bottom,wb.left,wb.bottom);
+        }
+        b.line(wb.left,wb.bottom,wb.left,wb.top);
+        b.line(wb.left,wb.top,wb.right,wb.top);
+        b.line(wb.right,wb.top,wb.right,wb.bottom);
+        wbp = wb;
+      });
+      b.line(wbp.right,wbp.bottom,b.width,wbp.bottom);
+    });
+  });
+}
+
+b.go();
