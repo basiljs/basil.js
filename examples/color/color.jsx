f751321be5a33929d689bfd03d509a4348c4caf5
Benedikt Gross <benedikt@looksgood.de>
2013-04-15 08:50:18 +0200

    added better comments to color example

--- examples/color/color.jsx
+++ examples/color/color.jsx
@@ -11,7 +11,7 @@ function draw() {
   // create RGB colors
   b.colorMode(b.RGB);
   var red = b.color(255,2,3);
-  var green = b.color(0,255,0,"green");
+  var green = b.color(0,255,0,"my green"); // set the name of the RBG color, see colour window
   var rgbGrey = b.color(128);
 
   b.stroke( rgbGrey );
@@ -24,8 +24,8 @@ function draw() {
   // create CMYK colors
   b.colorMode(b.CMYK);
   var magenta = b.color(1,100,3,4);
-  var yellow = b.color(0,0,100,0,"yellow");
-  var cmykGrey = b.color(80,"light grey");
+  var yellow = b.color(0,0,100,0,"my yellow"); // set the name of the CMYK color, see colour window
+  var cmykGrey = b.color(80,"my light grey");
 
   b.stroke( cmykGrey );
   b.fill( magenta );
@@ -33,10 +33,13 @@ function draw() {
   b.fill( yellow );
   b.rect(0,250,b.width,50);
 
-  // get a color from document
+  // get a color from the document via the name
+  b.fill( b.color("my green") );
+  b.rect(0,500,b.width,50); 
+  // a few colors like "Black" are predefined in every indesign document
   var black = b.color("Black");
   b.fill( black );
-  b.rect(0,500,b.width,50); 
+  b.rect(0,550,b.width,50);
 
   b.println(b.doc().swatches.length - swatchCount +" colors added to the swatches of the document");
 }
