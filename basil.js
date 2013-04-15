0a03c05a20f9b28c8e7abc3540586485d96fd9a3
Benedikt Gross <benedikt@looksgood.de>
2013-04-15 19:47:51 +0200

    better jsdoc desc for isText()

--- basil.js
+++ basil.js
@@ -953,6 +953,8 @@
 
   /**
    * Checks whether a var is an indesign text object, returns true if this is the case
+   * NB: a indesign TextFrame will return false as it is just a container holding text. 
+   * So you could say that isText() refers to all the things inside a TextFrame.
    *
    * @cat Document
    * @subcat Type-Check
