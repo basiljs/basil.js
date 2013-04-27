41768c8af9e29a59f2125e6d125c93d1822f6134
Benedikt Gross <benedikt@looksgood.de>
2013-04-27 12:37:17 +0100

    added new demo: how to create a bar chart from csv data

---
+++ examples/demos/B08_bar_chart_from_csv_data/bar_chart.jsx
@@ -0,0 +1,38 @@
+#includepath "~/Documents/;%USERPROFILE%Documents";
+#include "basiljs/bundle/basil.js";
+
+function draw() {
+  // get textframe which should hold the chart
+  var chartCtn = b.labels('chartContainer')[0];
+  var chartCtnWidth = b.itemWidth(chartCtn);
+
+  // load data
+  var poolsPerHood = b.CSV.decode( b.loadString("LA_Pools-Pools_per_Hood.csv") );
+  b.println("loaded "+poolsPerHood.length+" neighborhoods");
+
+  // set stroke parameters
+  b.strokeWeight(3);
+  b.colorMode(b.CMYK);
+  var barColor = b.color(56,16,20,0);
+  b.stroke(barColor);
+
+  // create barchart
+  for (var i = 0; i < poolsPerHood.length; i++) {
+    var name = poolsPerHood[i].hoodname;
+    var count = parseInt(poolsPerHood[i].poolcount);
+
+    var descTxt = name+" "+count;
+    b.println(i+" "+descTxt);
+
+    var barWidth = b.map(count,0,3000,1,chartCtnWidth);
+    var barLine = b.line(0,0,barWidth,0);
+    // add things to the story flow
+    b.addToStory(chartCtn.parentStory, barLine);
+    b.addToStory(chartCtn.parentStory, "\n");
+    b.addToStory(chartCtn.parentStory, descTxt);
+    b.addToStory(chartCtn.parentStory, "\n");
+  };
+}
+
+b.go(b.MODESILENT);
+//b.go();
