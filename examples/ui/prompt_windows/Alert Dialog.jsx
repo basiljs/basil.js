672849f776e645727e97eebf40385192e4ad1f9a
frederickk <ken.frederick@gmx.de>
2013-11-04 17:27:50 +0100

    cleaning up examples/ui

--- examples/ui/prompt_windows/Alert Dialog.jsx
+++
@@ -1,6 +0,0 @@
-////////////////////////////////////////////////////////////////////////////////
-// This script belongs to the following tutorial:
-//
-// http://scriptographer.org/tutorials/user-interface/displaying-dialog-windows/
-
-Dialog.alert('Don\'t Panic', 'As long as you know where your towel is.');
--- examples/ui/prompt_windows/Confirm Dialog.jsx
+++
@@ -1,9 +0,0 @@
-////////////////////////////////////////////////////////////////////////////////
-// This script belongs to the following tutorial:
-//
-// http://scriptographer.org/tutorials/user-interface/displaying-dialog-windows/
-
-var decision = Dialog.confirm('Do you want to know the Answer to the Ultimate Question of Life, the Universe, and Everything?');
-if (decision) {
-	Dialog.alert('The Answer is 42, what was the question again?');
-}
