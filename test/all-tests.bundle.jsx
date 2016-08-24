f7288830e15292afd46e4ddd88a380ef083fcdf2
fabiantheblind <icke@fabianmoronzirfas.me>
2016-08-24 12:02:48 +0200

    test(bundle): added test for basil.bundle.js all passing
    #39

---
+++ test/all-tests.bundle.jsx
@@ -0,0 +1,14 @@
+#include "../basil.bundle.js";
+#include "../lib/basil.test.js";
+
+#include "typography-tests.jsx";
+#include "transform-tests.jsx";
+#include "conversion-tests.jsx";
+#include "data-tests.jsx";
+#include "environment-tests.jsx";
+#include "input-tests.jsx";
+#include "math-tests.jsx";
+#include "string-tests.jsx";
+#include "color-tests.jsx";
+#include "vertex-tests.jsx";
+#include "group-tests.jsx";
