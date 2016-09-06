659fa86cc8a40ec18b7c8f9159c0e916c785cf75
fabianmoronzirfas <icke@fabianmoronzirfas.me>
2016-09-06 09:01:15 +0200

    added download folder to ignore files

---
+++ test/download/download.jsx
@@ -0,0 +1,8 @@
+﻿// @include "../../src/basil.js";
+/* global b */
+function setup () {
+b.download("https://raw.githubusercontent.com/basiljs/basil.js/master/changelog.txt");
+}
+function draw() {}
+b.go();
+
