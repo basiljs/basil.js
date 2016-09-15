4a3b38a8337f88c74034232377c9326958b14168
fabianmoronzirfas <icke@fabianmoronzirfas.me>
2016-09-15 11:28:27 +0200

    test(portable): ignore test files for portable

---
+++ test/loop/loop.jsx
@@ -0,0 +1,19 @@
+﻿//@targetengine "loop";
+//@include "./basil.js";
+var x = 0;
+var y = 0;
+function setup() {
+  b.doc();
+
+}
+
+function draw () {
+  b.ellipse(x, y, 10, 10);
+  x += 10;
+  if(x >= b.width) {
+    x = 0;
+    y += 10;
+  }
+}
+
+b.loop(25);
