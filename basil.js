9b759c0b6833d6ecd6450f5f76ec8d25979c8cd3
trych <timorychert@23x5.net>
2018-04-23 15:00:27 +0200

    Restructuring image.js

--- basil.js
+++ basil.js
@@ -5898,11 +5898,14 @@ pub.placeholder = function (textFrame) {
 // src/includes/image.js
 // ----------------------------------------
 
+// ----------------------------------------
+// Image
+// ----------------------------------------
+
 /**
  * @description Adds an image to the document. If the image argument is given as a string the image file must be in the document's data directory which is in the same directory where the document is saved in. The image argument can also be a File instance which can be placed even before the document was saved. The second argument can either be the `x` position of the frame to create or an instance of a rectangle, oval or polygon to place the image in. If an `x` position is given, a `y` position must be given, too. If `x` and `y` positions are given and width and height are not given, the frame's size gets set to the original image size.
  *
- * @cat     Document
- * @subcat  Image
+ * @cat     Image
  * @method  image
  *
  * @param   {String|File} img The image file name in the document's data directory or a File instance.
@@ -5994,8 +5997,7 @@ pub.image = function(img, x, y, w, h) {
 /**
  * @description Modifies the location from which images draw. The default mode is `CORNER`, which specifies the location to be the upper left corner and uses the fourth and fifth parameters of `image()` to set the image's width and height. The syntax `imageMode(CORNERS)` uses the second and third parameters of `image()` to set the location of one corner of the image and uses the fourth and fifth parameters to set the opposite corner. Use `imageMode(CENTER)` to draw images centered at the given `x` and `y` position. If no parameter is passed the currently set mode is returned as String.
  *
- * @cat     Document
- * @subcat  Image
+ * @cat     Image
  * @method  imageMode
  *
  * @param   {String} [mode] Either `CORNER`, `CORNERS`, or `CENTER`.
