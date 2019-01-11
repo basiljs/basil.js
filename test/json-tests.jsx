37439b94baabc025e2913e67110d920fd4f81b20
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2019-01-11 14:14:48 +0100

    Adding test for json

---
+++ test/json-tests.jsx
@@ -0,0 +1,35 @@
+/* globals assert */
+if (!$.global.VERSION) {
+  var basilTest = null;
+  // @include "../basil.js";
+}
+if (!basilTest) {
+  // @include "../lib/basil.test.js";
+}
+basilTest("DataTests", {
+
+  tearDown: function() {
+    close(SaveOptions.no);
+  },
+  testJSONFromString: function() {
+    var json = '{"foo":"bah"}';
+    var notjson =  '{"foo":"bah}';
+    assert(typeof (JSON.parse(json)) === 'object');
+    assert(JSON.parse('1.0000000000000001') === 1);
+    try{
+      JSON.parse(notjson);
+    }catch(e){
+      assert(true);
+    }
+  },
+  testJSONFromStringWithStrangeResult: function(){
+    $.write("JSON.parse('1.000000000000001') should give a result of ");
+    var parsed = JSON.parse('1.000000000000001');
+    $.writeln(parsed);
+    $.writeln("assert(JSON.parse('1.000000000000001') === 1.000000000000001) does...");
+    assert(JSON.parse('1.000000000000001') === 1.000000000000001);
+  }
+
+});
+
+basilTest.result();
