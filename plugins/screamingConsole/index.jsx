b0e258c3a53a3103210c459bc082500f1759c95a
trych <timorychert@23x5.net>
2019-12-04 01:53:40 +0100

    Added plugins/ to .gitignore

--- plugins/screamingConsole/index.jsx
+++
@@ -1,6 +0,0 @@
-pub.println = function() {
-  var msg = Array.prototype.slice.call(arguments).join(" ").toUpperCase() + "!!!!!";
-  $.writeln(msg);
-  if (progressPanel)
-    progressPanel.writeMessage(msg + "\n");
-};
