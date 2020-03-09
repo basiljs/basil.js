77711c04cc7848b6bc7a182b5fb7887942062326
trych <timorychert@23x5.net>
2020-03-09 14:17:38 +0100

    random(): handling of wrong inputs; error messages

--- src/includes/math.js
+++ src/includes/math.js
@@ -393,17 +393,21 @@ pub.noiseSeed = function(seed) {
  */
 pub.random = function() {
   if (arguments.length === 0) return currentRandom();
-  if (arguments.length === 1) {
-    if(arguments[0].length != undefined){
-      var argArray = Array.prototype.slice.call(arguments[0], 0);
-      return argArray[Math.floor(currentRandom() * argArray.length)];
+  var aMin = arguments[0];
+  var aMax = arguments[1];
+
+  if (isNumber(aMin)) {
+    if(isNumber(aMax)) {
+      return currentRandom() * (aMax - aMin) + aMin;
     } else {
-      return currentRandom() * arguments[0];
+      return currentRandom() * aMin;
     }
+  } else if(isArray(aMin) || aMin.hasOwnProperty("length") && isNumber(aMin.length)) {
+    var argArray = Array.prototype.slice.call(arguments[0], 0);
+    return argArray[Math.floor(currentRandom() * argArray.length)];
+  } else {
+    error("random(), wrong first argument. Needs to be a number or an array/collection.");
   }
-  var aMin = arguments[0],
-    aMax = arguments[1];
-  return currentRandom() * (aMax - aMin) + aMin;
 };
 
 /**
