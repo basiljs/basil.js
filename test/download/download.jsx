19a639540c7ab7c8a4200c1810b7474f5c378c5e
Romain Cazier <tools@romaincazier.com>
2022-02-19 09:57:01 +0100

    Updated tests for "b.download()"

--- test/download/download.jsx
+++ test/download/download.jsx
@@ -1,8 +1,28 @@
-﻿// @include "./basil.js";
-/* global b */
-function setup () {
-  b.download("https://raw.githubusercontent.com/basiljs/basil.js/master/changelog.txt");
+﻿if (typeof b === 'undefined') {
+  #include "../../basil.js";
 }
-function draw() {}
-b.go();
+if (typeof b.test === 'undefined') {
+  #include "../../lib/basil.test.js";  
+}
 
+b.test('download', {
+
+  testDownload: function() {
+    b.download("https://raw.githubusercontent.com/basiljs/basil.js/master/changelog.txt");
+    assert(new File("./download/changelog.txt"));
+  },
+
+  testDownloadWithQueryString: function() {
+    b.download("https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=https://basiljs.ch", "qrcode.png");
+    assert(new File("./qrcode.png"));
+  },
+  
+  testDownloadWithSpacesInPath: function() {
+    b.download("https://raw.githubusercontent.com/basiljs/basil.js/master/lib/basil.png", "folder with spaces/basil.png");
+    assert(new File("./folder with spaces/basil.png"));
+  }
+
+});
+
+// print collected test results
+b.test.result();
